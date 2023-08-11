3981d5cd079ebefa37ae8c83a2e6ad96b2f8a419
Freya <freyawaterbel@gmail.com>
2023-08-10 21:13:10 -0400

    studio 3 complete

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [ {label: "soup", value: "soup"}, {label: "vegan", value: "vegan"}, {label: "curry", value: "curry"}];
+
+   const [ boardName, setBoardName ] = useState('no boards yet!');
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         <option value="">Select a board</option>
+         {boards.map((boards) => {
+            return <option key={boards.value} value={boards.value}>{boards.label}</option>
+         })}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.foodbymaria.com";
+   let authorPhoto = "https://www.foodbymaria.com/wp-content/uploads/2019/07/Collages-FBM-ProfileSide.jpg";
+   let authorName = "FoodByMaria";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt="FoodByMaria" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["chopped white onion", "garlic", "bread crumbs", "olive oil", "oat milk", "spices", "fresh chopped mint and green onions"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -25,6 +25,8 @@ const RecipeIngredients = () => {
             <li>{ingredients[2]}</li>
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
+            <li>{ingredients[5]}</li>
+            <li>{ingredients[6]}</li>
          </ul>
       </div>
    );
@@ -34,8 +36,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Plant-Based Coconut Curry Meatballs With Soup</h1>
+            <p>This yummy, creamy Plant-Based Coconut Curry Meatballs Soup is to die for. Loaded with flavour, and filling.</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +49,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://www.foodbymaria.com/wp-content/uploads/2020/04/Plant-Based-Coconut-Curry-Meatballs-4.jpg" alt="Plant-Based Coconut Curry Meatballs With Soup" className="imageUpdates"/>
    );
 }
 
